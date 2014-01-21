468420073449ec2d298fcee1ad127ffc096d53ea
Rick Barber <barber5@stanford.edu>
2014-01-21 15:59:15 -0800

    no message

--- relatedTerms.py
+++ relatedTerms.py
@@ -7,11 +7,12 @@ def related_terms(term_query):
 	i1 = Isaclosure.alias()
 	i2 = Isaclosure.alias()
 	st = Str2tid.alias()
-	tms = Terms.select(Terms, st.alias('st')).where(Terms.term == term_query).join(t1, on=(t1.tid==Terms.tid)).join(i1, on=(t1.cid==i1.cid1)).join(i2, on=(i1.cid1==i2.cid2)).join(t2, on=(t2.cid==i2.cid2)).join(st, on=(t2.tid==st.tid)).limit(20)
+	tms = (Terms.select(Terms, st).where(Terms.term == term_query).join(t1, on=(t1.tid==Terms.tid)).join(i1, on=(t1.cid==i1.cid1)).join(i2, on=(i1.cid1==i2.cid2)).join(t2, on=(t2.cid==i2.cid2)).join(st, on=(t2.tid==st.tid)).limit(20))
 	print tms
 	res = tms.execute()
 	for r in res:
 		print dir(r)
+		print r.st
 		print r.str	
 
 if __name__ == "__main__":
