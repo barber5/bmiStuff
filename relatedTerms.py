4cd6b385959fae3c74053f5e6aa58f100c897068
Rick Barber <barber5@stanford.edu>
2014-01-21 15:06:27 -0800

    no message

--- relatedTerms.py
+++ relatedTerms.py
@@ -3,7 +3,7 @@ import sys
 
 def related_terms(term_query):
 	Isa2 = Isaclosure.alias()
-	tms = Terms.select().where(Terms.term == term_query).join(Tid2cid.join(Isaclosure.join(Isa2, on=(Isa2.cid2=Isaclosure.cid1)), on=(Tid2cid.cid=Isaclosure.cid1)), on=(Tid2cid.tid==Terms.tid))
+	tms = Terms.select().where(Terms.term == term_query).join(Tid2cid.join(Isaclosure.join(Isa2, on=(Isa2.cid2==Isaclosure.cid1)), on=(Tid2cid.cid==Isaclosure.cid1)), on=(Tid2cid.tid==Terms.tid))
 	print tms
 
 if __name__ == "__main__":
