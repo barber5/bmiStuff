a59716d5b5cd0d4c30e76c7b0a376f0948a20e30
Rick Barber <barber5@stanford.edu>
2014-01-21 14:00:17 -0800

    no message

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ from fabric.api import *
 from fabric.context_managers import cd
 from fabric.contrib.files import append
 
-
+env.hosts = ['barber5@ncbolabs-dev4.stanford.edu']
 
 @task
 def deploy(message="no message", name="ricktest"):                  
@@ -13,6 +13,7 @@ def deploy(message="no message", name="ricktest"):
     with settings(warn_only=True): 
         local('git commit -m "%s"' % message)
     local('git pull origin master && git push origin master')        
+
     with cd('bmiStuff'):                
         run('git fetch')
         run('git reset --hard origin/master')
