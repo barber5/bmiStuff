dad83238045cd361ef29bec72b444cf9a94c27d0
Rick Barber <barber5@stanford.edu>
2014-01-21 13:58:59 -0800

    no message

---
+++ fabfile.py
@@ -0,0 +1,19 @@
+import sys
+import time
+import fabric.exceptions
+from fabric.api import *
+from fabric.context_managers import cd
+from fabric.contrib.files import append
+
+
+
+@task
+def deploy(message="no message", name="ricktest"):                  
+    local('git add .')
+    with settings(warn_only=True): 
+        local('git commit -m "%s"' % message)
+    local('git pull origin master && git push origin master')        
+    with cd('bmiStuff'):                
+        run('git fetch')
+        run('git reset --hard origin/master')
+        
