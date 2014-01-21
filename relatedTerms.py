75c18bcc4b3b328d36c95390caf29b762562ae50
Rick Barber <barber5@stanford.edu>
2014-01-21 15:33:35 -0800

    no message

--- relatedTerms.py
+++ relatedTerms.py
@@ -2,9 +2,15 @@ from models import *
 import sys
 
 def related_terms(term_query):
-	Isa2 = Isaclosure.alias()
-	tms = Terms.select().where(Terms.term == term_query).join(Tid2cid.select().join(Isaclosure.select().join(Isa2, on=(Isa2.cid2==Isaclosure.cid1)), on=(Tid2cid.cid==Isaclosure.cid1)), on=(Tid2cid.tid==Terms.tid))
-	print tms
+	t1 = Tid2cid.alias()
+	t2 = Tid2cid.alias()
+	i1 = Isaclosure.alias()
+	i2 = Isaclosure.alias()
+	st = Str2tid.alias()
+	tms = Terms.select().where(Terms.term == term_query).join(t1, on=(t1.tid==Terms.tid)).join(i1, on=(t1.cid==i1.cid1)).join(i2, on=(i1.cid1==i2.cid2)).join(t2, on=(t2.cid==i2.cid2)).join(st, on=(t2.tid==st.tid)).distinct(Terms.term).limit(20)
+	res = tms.get()
+	for r in res:
+		print res.term	
 
 if __name__ == "__main__":
 	rt = related_terms(sys.argv[1])
