2d62cf3546624fc78b54e51560ec2c80dc622ca4
Rick Barber <barber5@stanford.edu>
2014-01-21 14:31:05 -0800

    no message

---
+++ db.py
@@ -0,0 +1,21 @@
+from peewee import *
+
+
+
+def getDbs():
+	with open('/home/barber5/.my.cnf', 'r') as f:
+		for line in f:
+			if line.find('user') == 0:
+				user = line.split()[-1]
+			if line.find('password') == 0:
+				password = line.split()[-1]
+			if line.find('host') == 0:
+				host = line.split()[-1]
+		print user
+		print host
+		print password
+
+		term_db = MySQLDatabase('terminology3', user=user, host=host, password=password)
+		stride_db = MySQLDatabase('stride5', user=user, host=host, password=password)
+
+		return term_db, stride_db
--- models.py
+++ models.py
@@ -1,17 +1,13 @@
 from peewee import *
+from utils import getDbs
+term_db, stride_db = getDbs()
 
-mysql_db = None
-with open('/home/barber5/.my.cnf', 'r') as f:
-	for line in f:
-		if line.find('user') == 0:
-			user = line.split()[-1]
-		if line.find('password') == 0:
-			password = line.split()[-1]
-		if line.find('host') == 0:
-			host = line.split()[-1]
-	print user
-	print host
-	print password
+class Term(Model):
+    ontology = CharField()
+    termid = CharField()
+    term = CharField()
+    cui = CharField()
+    tid = IntegerField()
 
-	term_db = MySQLDatabase('terminology3', user=user, host=host, password=password)
-	stride_db = MySQLDatabase('stride5', user=user, host=host, password=password)
+    class Meta:
+        database = term_db # this model uses the people database
