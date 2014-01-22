69d6ed76e9d7184c9c99e47dbca9f8f7385af459
Rick Barber <barber5@stanford.edu>
2014-01-21 16:49:15 -0800

    no message

--- relatedTerms.py
+++ relatedTerms.py
@@ -1,5 +1,5 @@
 from db import *
-import sys
+import sys, pprint
 
 
 (term_db, stride_db) = getDbs()
@@ -8,9 +8,17 @@ def related_terms(term_query):
 	query = "SELECT s1.tid as tid, s1.str as str, t2.cid as cid FROM terms AS t1 INNER JOIN tid2cid AS t2 ON (t2.tid = t1.tid) INNER JOIN isaclosure AS i1 ON (t2.cid = i1.cid1) INNER JOIN isaclosure AS i2 ON (i1.cid1 = i2.cid2) INNER JOIN tid2cid AS t4 ON (t4.cid = i2.cid2) INNER JOIN str2tid AS s1 ON (t4.tid = s1.tid) WHERE (t1.term = %s) group by s1.tid"
 
 	rows = tryQuery(term_db, query, [term_query])
+	result = []
 	for row in rows:
-		print row
+		d = {
+			'tid': row[0],
+			'term': row[1],
+			'cid': row[2]
+		}
+		result.append(d)
+	return result
 
 	
 if __name__ == "__main__":
 	rt = related_terms(sys.argv[1])	
+	pprint.pprint(rt)
