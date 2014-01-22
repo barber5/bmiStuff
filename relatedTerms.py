14227d2e43cb066f34c8690eec0a11fa6fb88059
Rick Barber <barber5@stanford.edu>
2014-01-21 16:49:48 -0800

    no message

--- relatedTerms.py
+++ relatedTerms.py
@@ -5,7 +5,7 @@ import sys, pprint
 (term_db, stride_db) = getDbs()
 
 def related_terms(term_query):
-	query = "SELECT s1.tid as tid, s1.str as str, t2.cid as cid FROM terms AS t1 INNER JOIN tid2cid AS t2 ON (t2.tid = t1.tid) INNER JOIN isaclosure AS i1 ON (t2.cid = i1.cid1) INNER JOIN isaclosure AS i2 ON (i1.cid1 = i2.cid2) INNER JOIN tid2cid AS t4 ON (t4.cid = i2.cid2) INNER JOIN str2tid AS s1 ON (t4.tid = s1.tid) WHERE (t1.term = %s) group by s1.tid"
+	query = "SELECT s1.tid as tid, s1.str as str, t4.cid as cid FROM terms AS t1 INNER JOIN tid2cid AS t2 ON (t2.tid = t1.tid) INNER JOIN isaclosure AS i1 ON (t2.cid = i1.cid1) INNER JOIN isaclosure AS i2 ON (i1.cid1 = i2.cid2) INNER JOIN tid2cid AS t4 ON (t4.cid = i2.cid2) INNER JOIN str2tid AS s1 ON (t4.tid = s1.tid) WHERE (t1.term = %s) group by s1.tid"
 
 	rows = tryQuery(term_db, query, [term_query])
 	result = []
