87c21a2a4dc5381e0ccf4605a8ccc13b5d5f1b17
Rick Barber <barber5@stanford.edu>
2014-01-21 21:35:47 -0800

    no message

--- db.py
+++ db.py
@@ -26,3 +26,71 @@ def tryQuery(db, query, replace=None):
 		c.execute(query)
 	return c.fetchall()
 
+
+def extractIt(row, mapping, result, idxs, attrName):   
+    #print 'ROW IS {}'.format(row) 
+    if attrName and attrName not in idxs:
+        idxs[attrName] = {}
+    if type(result) == type([]):
+        pk = getPkFromMapping(row, mapping)
+        #print 'PK is {}, attrName is {}'.format(pk, attrName)        
+        if pk and pk not in idxs[attrName]:            
+            idxs[attrName][pk] = len(result)
+            result.append({})    
+            #print 'appended {} for PK'        
+        if pk:
+            pkindex = idxs[attrName][pk]
+            #print 'pkindex is {}'.format(pkindex)
+            #print 'into this result {}'.format(result)
+            if hasSublist(mapping):
+                attrName = str(pk)+attrName
+            extractIt(row, mapping, result[pkindex], idxs, attrName)
+
+    elif type(result) == type({}):
+        pk = getPkFromMapping(row, mapping)
+        if hasSublist(mapping):
+            if not attrName:
+                attrName = ''
+            attrName = str(pk)+attrName
+        for k,v in mapping.iteritems():
+            if(type(k) == type(23)):
+                if v not in result:
+                    result[v] = row[k]
+            elif type(k) == type('fnord'):
+                if k not in result:
+                    if type(v) == type({}):
+                        result[k] = {}
+                    elif type(v) == type([]):
+                        result[k] = []
+                    else:
+                        raise Exception('bad name mapping for joiner')
+                if type(v) == type({}):
+                    extractIt(row, v, result[k], idxs, attrName)
+                elif type(v) == type([]):
+                    if not attrName:
+                        attrName = ''
+                    extractIt(row, v[0], result[k], idxs, k + attrName)
+                else:
+                    raise Exception('bad name mapping for joiner')
+    else:
+        raise Exception('bad name mapping for joiner')
+
+    
+
+
+def joinResult(rows, nameMapping):       
+    result = {}
+    idxs = {}
+    attrName = None
+    if len(nameMapping) == 1:
+        key = nameMapping.keys()[0]
+        if type(nameMapping[key]) == type([]):
+            attrName = key
+            if len(rows) == 0:
+                result[key] = []
+    for row in rows:
+        extractIt(row, nameMapping, result, idxs, attrName)    
+    #print 'joinResult result: {}'.format(result)
+    return result
+
+
--- vecForPatient.py
+++ vecForPatient.py
@@ -4,23 +4,79 @@ import sys,pprint
 (term_db, stride_db) = getDbs()
 
 def getPatientVec(pid):
-	query = "SELECT n.nid, n.timeoffset, m.tid FROM note as n inner join mgrep as m on n.nid=m.nid WHERE n.pid=%s"
+	query = "SELECT n.pid, n.patient, n.nid, n.src, n.src_type, n.age, n.timeoffset, n.year, n.duration, n.cpt, n.icd9, m.tid, m.negated, m.familyHistory, t.term, v.visit, v.src, v.src_type, v.age, v.timeoffset, v.year, v.duration, v.cpt, v.icd9, v.vid, l.lid, l.src, l.age, l.timeoffset, l.description, l.proc, l.proc_cat, l.line, l.component, l.ord, l.ord_num, l.result_flag, l.ref_low, l.ref_high, l.ref_unit, l.result_inrange, l.ref_norm, p.rxid, p.src, p.age, p.timeoffset, p.drug_description, p.route, p.order_status, p.ingr_set_id, d.gender, d.race, d.ethnicity, d.death FROM note as n inner join mgrep as m on n.nid=m.nid inner join visit as v on v.pid=n.pid inner join prescription p on p.pid=n.pid inner join lab l on l.pid=n.pid inner join demographics d on d.pid=n.pid inner join terminology3.terms as t on t.tid=n.tid WHERE n.pid=%s"
 	rows = tryQuery(stride_db, query, [pid])
-	notes = {}
-	for row in rows:
-		nid = row[0]
-		if nid not in notes:
-			notes[nid] = {
-				'nid': nid,
-				'offset': row[1],
-				'terms': []
-			}
-		notes[nid]['terms'].append(row[2])
-	return {
-		'pid': pid,
-		'notes': notes.values()
+	nameMapping = {
+		'patients': [{
+			0: 'pid',
+			1: 'patient',
+			50: 'gender',
+			51: 'race',
+			52: 'ethnicity',
+			53: 'death',
+			'notes': [{
+				2: 'nid',
+				3: 'src',
+				4: 'src_type',
+				5: 'age',
+				6: 'timeoffset',
+				7: 'year',
+				8: 'duration',
+				9: 'cpt',
+				10: 'icd9'
+				'terms': [{
+					11: 'tid',
+					12: 'negated',
+					13: 'familyHistory',
+					14: 'term'
+				}]
+			}],
+			'visits': [{
+				15: 'visit',
+				16: 'src',
+				17: 'src_type',
+				18: 'age',
+				19: 'timeoffset',
+				20: 'year',
+				21: 'duration',
+				22: 'cpt',
+				23: 'icd9',
+				24: 'vid'
+			}],
+			'labs': [{
+				25: 'lid',
+				26: 'src',
+				27: 'age',
+				28: 'timeoffset',
+				29: 'description',
+				30: 'proc',
+				31: 'proc_cat',
+				32: 'line',
+				33: 'component',
+				34: 'ord',
+				35: 'ord_num',
+				36: 'result_flag',
+				37: 'ref_low',
+				38: 'ref_high',
+				39: 'ref_unit',
+				40: 'result_inrange',
+				41: 'ref_norm'
+			}],
+			'prescriptions': [{
+				42: 'rxid',
+				43: 'src',
+				44: 'age',
+				45: 'timeoffset',
+				46: 'drug_description',
+				47: 'route',
+				48: 'order_status',
+				49: 'ingr_set_id'
+			}]
+		}]
 	}
-
+	result = joinResult(rows, nameMapping)
+	return result
+	
 def getMultiplePatientVec(pids):
 	result = []
 	for pid in pids:
