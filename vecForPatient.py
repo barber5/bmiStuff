730d07aa10e1ee509553323c0712681f899dc8bb
Rick Barber <barber5@stanford.edu>
2014-01-21 17:36:58 -0800

    no message

--- vecForPatient.py
+++ vecForPatient.py
@@ -4,9 +4,25 @@ import sys,pprint
 (term_db, stride_db) = getDbs()
 
 def getPatientVec(pid):
-	query = "SELECT * FROM note as n inner join mgrep as m on n.nid=m.nid WHERE n.pid=%s AND m.tid > 0"
+	query = "SELECT n.nid, n.timeoffset, m.tid FROM note as n inner join mgrep as m on n.nid=m.nid WHERE n.pid=%s"
 	rows = tryQuery(stride_db, query, [pid])
-	print rows[0]
+	notes = {}
+	for row in rows:
+		nid = row[0]
+		if nid not in notes:
+			notes[nid] = {
+				'nid': nid,
+				'offset': row[1],
+				'terms': []
+			}
+		notes[nid]['terms'].append(row[2])
+	return {
+		'pid': pid,
+		'notes': notes.values()
+	}
+
+	
 
 if __name__ == "__main__":
-	getPatientVec(sys.argv[1])
+	vec = getPatientVec(sys.argv[1])
+	pprint.pprint(vec)
