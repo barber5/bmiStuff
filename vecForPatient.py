611ad2e1fbc926b0fb5603988083fc7c8e12df77
Rick Barber <barber5@stanford.edu>
2014-01-21 17:30:56 -0800

    no message

--- vecForPatient.py
+++ vecForPatient.py
@@ -6,7 +6,7 @@ import sys,pprint
 def getPatientVec(pid):
 	query = "SELECT * FROM note as n inner join mgrep as m on n.nid=m.nid WHERE n.pid=%s"
 	rows = tryQuery(stride_db, query, [pid])
-	print len(rows)
+	print rows
 
 if __name__ == "__main__":
 	getPatientVec(sys.argv[1])
