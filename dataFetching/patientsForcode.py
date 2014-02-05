9a1d323e78616e3c96e8a1dbeaf17fd76f2c784f
Rick Barber <barber5@stanford.edu>
2014-02-04 16:14:28 -0800

    no message

--- dataFetching/patientsForcode.py
+++ dataFetching/patientsForcode.py
@@ -15,8 +15,9 @@ def getPids(icd9):
 def getVisits(pids, src_type=None):
 	result = []
 	for i, pid in enumerate(pids):		
-		print >> sys.stderr, 'working on visit %s of %s, pid %s' % (i, len(pids), pid)
-		query = "SELECT pid, age, timeoffset, year, icd9 FROM visit WHERE pid=%s"
+		if i%10 == 0:
+			print >> sys.stderr, 'working on visits %s of %s, pid %s' % (i, len(pids), pid)
+		query = "SELECT pid, src, src_type, age, timeoffset, year, duration, cpt, icd9 FROM visit WHERE pid=%s"
 		repls = [int(pid)]
 		if src_type:			
 			query += " AND src_type=%s"
@@ -29,8 +30,9 @@ def getVisits(pids, src_type=None):
 def getNoteIds(pids, src_type=None):
 	result = []
 	for pid in pids:
-		print >> sys.stderr, 'working on note {} of {} (pid: {})'.format(i, len(pids), pid)
-		query = "SELECT pid, nid, src, src_type, age, timeoffset, year, duration, cpt, icd9 FROM note where pid=%s"
+		if i%10 == 0:
+			print >> sys.stderr, 'working on notes %s of %s, pid %s' % (i, len(pids), pid)
+		query = "SELECT pid, src, src_type, age, timeoffset, year, duration, cpt, icd9 FROM note where pid=%s"
 		repls = [int(pid)]
 		if src_type:			
 			query += " AND src_type=%s"
@@ -44,12 +46,25 @@ def getNoteIds(pids, src_type=None):
 def getPrescriptions(pids, src_type=None):
 	result = []
 	for pid in pids:
-		print >> sys.stderr, 'working on prescription {} of {} (pid: {})'.format(i, len(pids), pid)
-		query = "SELECT pid, rxid, src, age, offset,  where pid=%s"
+		if i%10 == 0:
+			print >> sys.stderr, 'working on prescriptions %s of %s, pid %s' % (i, len(pids), pid)
+		query = "SELECT pid, rxid, src, age, timeoffset, drug_description, route, order_status, ingr_set_id FROM prescription where pid=%s"
 		repls = [int(pid)]
 		if src_type:			
 			query += " AND src_type=%s"
 			repls.append(src_type)
+		rows = tryQuery(stride_db, query, repls)
+		for row in rows:	
+			result.append(row)
+	return result
+
+def getLabs(pids):
+	result = []
+	for pid in pids:
+		if i%10 == 0:
+			print >> sys.stderr, 'working on labs %s of %s, pid %s' % (i, len(pids), pid)
+		query = "SELECT l.lid, l.src, l.age, l.timeoffset, l.description, l.proc, l.proc_cat, l.line, l.component, l.ord, l.ord_num, l.result_flag, l.ref_low, l.ref_high, l.ref_unit, l.result_inrange, l.ref_norm from lab as l where l.pid=%s"
+		repls = [int(pid)]		
 		rows = tryQuery(stride_db, query, repls)
 		for row in rows:	
 			result.append(row)
@@ -61,6 +76,7 @@ def getFullPatients(code, src_type):
 	visits = getVisits(pids, src_type)
 	notes = getNoteIds(pids, src_type)
 	prescriptions = getPrescriptions(pids, src_type)
+	labs = getLabs(pids)
 
 def getCodedVisitsOnly(code, src_type):
 	pids = getPids(code)
@@ -73,7 +89,8 @@ if __name__ == "__main__":
 		src_type = sys.argv[2]
 	else:
 		src_type = None
-	getCodedVisitsOnly(sys.argv[1], src_type)
+	#getCodedVisitsOnly(sys.argv[1], src_type)
+	getFullPatients(sys.argv[1], src_type)
 
 
 
