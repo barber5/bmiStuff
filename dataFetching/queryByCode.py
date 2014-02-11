09becf3f6ada39989d8329337f1376e56da82abc
Rick Barber <barber5@stanford.edu>
2014-02-11 14:55:14 -0800

    no message

--- dataFetching/queryByCode.py
+++ dataFetching/queryByCode.py
@@ -1,5 +1,5 @@
 from db import *
-import sys, pprint, threading
+import sys, pprint, threading, pickle
 
 
 
@@ -158,22 +158,96 @@ class myThread (threading.Thread):
 def printAndGetFull(code, src_type, filePrefix):
 	pids = getPids(code, src_type)
 	print 'initializing threads'
-	vt = myThread('visits', pids, filePrefix, src_type)
-	
-	nt = myThread('notes', pids, filePrefix, src_type)
-	
-	pt = myThread('prescriptions', pids, filePrefix, src_type)
-	
-	lt = myThread('labs', pids, filePrefix, src_type)
-	
+	vt = myThread('visits', pids, filePrefix, src_type)	
+	nt = myThread('notes', pids, filePrefix, src_type)	
+	pt = myThread('prescriptions', pids, filePrefix, src_type)	
+	lt = myThread('labs', pids, filePrefix, src_type)	
 	print 'threads initialized'
 	vt.start()
 	nt.start()
 	pt.start()
-	lt.start()
-
-	
+	lt.start()	
 	print 'done done done'
+
+def getSingleVisits(pid, src_type=None):
+	(term_db, stride_db) = getDbs()
+	query = "SELECT pid, age, timeoffset, year, icd9, src, src_type, duration, cpt FROM visit WHERE pid=%s"
+	repls = [int(pid)]
+	if src_type:			
+		query += " AND src_type=%s"
+		repls.append(src_type)
+	rows = tryQuery(stride_db, query, repls)
+	return rows
+
+def getSingleNotes(pid, src_type=None):
+	(term_db, stride_db) = getDbs()
+	query = "SELECT pid, nid, src, src_type, age, timeoffset, year, duration, cpt, icd9 FROM note where pid=%s"
+	repls = [int(pid)]
+	if src_type:			
+		query += " AND src_type=%s"
+		repls.append(src_type)		
+	rows = tryQuery(stride_db, query, repls)
+	result = []
+	for row in rows:	
+		rowStr = [str(i) for i in row]
+		result.append(rowStr)
+	return result
+
+def getSinglePrescriptions(pid, src_type=None):
+	(term_db, stride_db) = getDbs()
+	query = "SELECT pid, rxid, src, age, timeoffset, drug_description, route, order_status, ingr_set_id FROM prescription where pid=%s"		
+	repls = [int(pid)]
+	if src_type:			
+		query += " AND src_type=%s"
+		repls.append(src_type)
+	rows = tryQuery(stride_db, query, repls)
+	return rows
+
+def getSingleLabs(pid):
+	(term_db, stride_db) = getDbs()
+	query = "SELECT l.lid, l.src, l.age, l.timeoffset, l.description, l.proc, l.proc_cat, l.line, l.component, l.ord, l.ord_num, l.result_flag, l.ref_low, l.ref_high, l.ref_unit, l.result_inrange, l.ref_norm from lab as l where l.pid=%s"
+	repls = [int(pid)]		
+	rows = tryQuery(stride_db, query, repls)
+	return rows
+
+def writeSinglePatientFile(pat, pid, filePrefix):
+	fi = open(filePrefix+pid+'.txt', 'w')
+	fi.write(pat)
+	fi.close()
+	fi = open(filePrefix+pid+'.pkl', 'wb')
+	pickle.dump(pat, fi)
+	fi.close()
+
+
+class patientThread(threading.Thread):
+	def __init__(self, pid, filePrefix, src_type=None):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.pid = pid
+        self.filePrefix = filePrefix
+        self.src_type = src_type
+        
+    def run(self):		
+		visits = getVisits(self.pid, self.src_type)		
+		notes = getNoteIds(self.pid, self.src_type)		
+		prescriptions = getPrescriptions(self.pid, self.src_type)		
+		labs = getLabs(self.pid)		
+		patient = {
+			'pid': self.pid,
+			'src_type': self.src_type,
+			'visits': visits,
+			'notes': notes,
+			'prescriptions': prescriptions,
+			'labs': labs
+		}
+		writeSinglePatientFile(patient, self.pid, self.filePrefix)
+		
+
+
+def parallelPatients(code, src_type, filePrefix):
+	pids = getPids(code, src_type)
+	for pid in pids:
+		pt = patientThread(pid, filePrefix, src_type)
 
 if __name__ == "__main__":
 	if len(sys.argv) > 3:
