7f6d5ca77ee10662b9fa9ac9860ac8f5cc465530
Rick Barber <barber5@stanford.edu>
2014-02-11 15:45:22 -0800

    no message

--- dataFetching/queryByCode.py
+++ dataFetching/queryByCode.py
@@ -221,8 +221,7 @@ def writeSinglePatientFile(pat, pid, filePrefix):
 
 class patientThread(threading.Thread):
 	def __init__(self, pid, filePrefix, src_type=None):
-		threading.Thread.__init__(self)
-        self.name = name
+		threading.Thread.__init__(self)        
         self.pid = pid
         self.filePrefix = filePrefix
         self.src_type = src_type
