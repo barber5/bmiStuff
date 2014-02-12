081511441d42b45d6a297954f5252e586fd06287
Rick Barber <barber5@stanford.edu>
2014-02-11 17:43:32 -0800

    no message

--- dataFetching/db.py
+++ dataFetching/db.py
@@ -14,10 +14,10 @@ def getDbs():
                 with MySQLdb.connect(passwd=password,db="terminology3", host=host, user=user) as term_db:
                     with MySQLdb.connect(passwd=password,db="stride5", host=host, user=user) as stride_db:
 
-            		#term_db = MySQLDatabase('terminology3', user=user, host=host, passwd=password)
-            		#stride_db = MySQLDatabase('stride5', user=user, host=host, passwd=password)
+                        #term_db = MySQLDatabase('terminology3', user=user, host=host, passwd=password)
+                        #stride_db = MySQLDatabase('stride5', user=user, host=host, passwd=password)
 
-            		return term_db, stride_db
+                        return term_db, stride_db
             except Exception as e:
                 time.sleep(.1)
 
