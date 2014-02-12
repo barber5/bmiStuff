457a576744db981912e357762216723535c50ebf
Rick Barber <barber5@stanford.edu>
2014-02-11 17:42:31 -0800

    no message

--- dataFetching/db.py
+++ dataFetching/db.py
@@ -4,15 +4,15 @@ def getDbs():
     with open('/home/barber5/.my.cnf', 'r') as f:
         while True:
             try:
-        		for line in f:
-        			if line.find('user') == 0:
-        				user = line.split()[-1]
-        			if line.find('password') == 0:
-        				password = line.split()[-1]
-        			if line.find('host') == 0:
-        				host = line.split()[-1]		
-        		with MySQLdb.connect(passwd=password,db="terminology3", host=host, user=user) as term_db:
-        		  with MySQLdb.connect(passwd=password,db="stride5", host=host, user=user) as stride_db:
+                for line in f:
+                    if line.find('user') == 0:
+                        user = line.split()[-1]
+                    if line.find('password') == 0:
+                        password = line.split()[-1]
+                    if line.find('host') == 0:
+                        host = line.split()[-1]		
+                with MySQLdb.connect(passwd=password,db="terminology3", host=host, user=user) as term_db:
+                    with MySQLdb.connect(passwd=password,db="stride5", host=host, user=user) as stride_db:
 
             		#term_db = MySQLDatabase('terminology3', user=user, host=host, passwd=password)
             		#stride_db = MySQLDatabase('stride5', user=user, host=host, passwd=password)
