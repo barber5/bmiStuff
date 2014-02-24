d73f5fb673bc4aa20109ce0be68c8b86f97ffeb8
Rick Barber <barber5@stanford.edu>
2014-02-24 15:10:23 -0800

    no message

--- dataFetching/randomVisits.py
+++ dataFetching/randomVisits.py
@@ -1,15 +1,15 @@
-from patientsForcode import getVisits
+from vecForPatient import getVecForPid
 from random import randint
 import sys
 
-def getRandomVisits(num, src_type):
+def getRandomVisits(num):
 	visits = []
 	for i in range(num):
 		pid = randint(1, 1257139)
-		visits.extend(getVisits([pid], src_type))
+		visits.extend(getVecForPid([pid]))
 	return visits
 
 if __name__ == "__main__":
-	visits = getRandomVisits(int(sys.argv[1]), sys.argv[2])
+	visits = getRandomVisits(int(sys.argv[1]))
 	for v in visits:
 		print v
