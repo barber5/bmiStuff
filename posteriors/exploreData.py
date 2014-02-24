ff4b3f874c9f0118c250468daabe77c2af1e9f13
Rick Barber <barber5@stanford.edu>
2014-02-24 15:33:40 -0800

    no message

--- posteriors/exploreData.py
+++ posteriors/exploreData.py
@@ -3,11 +3,15 @@ sys.path.append(os.path.realpath('../tempClustering'))
 sys.path.append(os.path.realpath('./tempClustering'))
 sys.path.append(os.path.realpath('../dataFetching'))
 sys.path.append(os.path.realpath('./dataFetching'))
-from queryByCode import getPids
+from queryByCode import getPids, r
 
 def expForCode(code):
 	pids = getPids(code)
-	print pids
+	result = {}
+	for pid in pids:
+		if r.exists(pid):
+			result[pid] = json.loads(r.get(pid))
+	return result
 
 if __name__ == "__main__":
 	expForCode(sys.argv[1])
