84217456533f0d3e684563ed74b0579bffb30fb7
Rick Barber <barber5@stanford.edu>
2014-02-24 14:58:03 -0800

    no message

--- dataFetching/vecForPatient.py
+++ dataFetching/vecForPatient.py
@@ -1,5 +1,6 @@
 from queryByCode import getSingleVisits, getSingleVisits, getSinglePrescriptions, getSingleLabs, writeSinglePatientFile
 import sys
+from util import getDbs
 
 
 def getVecForPid(pid):
