2776de995fc770c871520bf2128253bf60fdee0e
Rick Barber <barber5@stanford.edu>
2014-02-26 15:48:44 -0800

    no message

---
+++ dataFetching/getTermByID.py
@@ -0,0 +1,14 @@
+from db import *
+import sys, pprint
+
+
+(term_db, stride_db) = getDbs()
+
+def getTerm(term_id):
+	query = "SELECT term from terms where termid=%s"
+	rows = tryQuery(term_db, query, [term_id])
+	print rows
+
+
+if __name__ == "__main__":
+	getTerm(sys.argv[1])
