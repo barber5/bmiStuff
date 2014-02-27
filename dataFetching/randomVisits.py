1b8a9d6f6da7135b4ee35f7a8e74a78defafd8ea
Rick Barber <barber5@stanford.edu>
2014-02-26 20:36:14 -0800

    no message

--- dataFetching/randomVisits.py
+++ dataFetching/randomVisits.py
@@ -1,5 +1,5 @@
 from vecForPatient import getVecForPid
-from queryByCode import r
+from queryByCode import r, compIt
 from random import randint
 import sys, json
 
@@ -10,7 +10,7 @@ def getRandomVisits(num):
 		pid = randint(1, 1257139)
 		pids.append(pid)
 		getVecForPid(pid, 'random')
-	r.hset('codes', 'random', json.dumps(pids))
+	r.hset('codes', 'random', compItjson.dumps(pids)))
 
 if __name__ == "__main__":
 	getRandomVisits(int(sys.argv[1]))
