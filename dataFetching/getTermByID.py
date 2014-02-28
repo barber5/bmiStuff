25284fb85ffdace1f48316611d28db43ffe4036b
Rick Barber <barber5@stanford.edu>
2014-02-27 17:59:45 -0800

    no message

--- dataFetching/getTermByID.py
+++ dataFetching/getTermByID.py
@@ -9,6 +9,10 @@ def getTerm(term_id):
 	rows = tryQuery(term_db, query, [term_id])
 	return rows[0][0]
 
+def getTermCui(cui):
+	query = "SELECT str from str2cid where cui=%s"
+	rows = tryQuery(term_db, query, [cui])
+	return rows[0][0]
 
 if __name__ == "__main__":
 	getTerm(sys.argv[1])
