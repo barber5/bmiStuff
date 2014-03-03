b3788c00f431625a068f577df8336f224f420c13
Rick Barber <barber5@stanford.edu>
2014-03-02 23:56:20 -0800

    no message

--- dataFetching/queryByCui.py
+++ dataFetching/queryByCui.py
@@ -15,7 +15,8 @@ def decomp(res):
 
 def getCuis(queryTerm, src_type=None):
 	(term_db, stride_db) = getDbs()
-	query = "SELECT distinct cid from terms t1 inner join tid2cid tc1 on t1.tid=tc1.tid where t1.term like '%%"+queryTerm+"%%'"
+	query = "SELECT distinct tid,term from terms t1 where t1.term like '%%"+queryTerm+"%%'"
+	#query = "SELECT distinct cid from terms t1 inner join tid2cid tc1 on t1.tid=tc1.tid where t1.term like '%%"+queryTerm+"%%'"
 	repls = []
 	if src_type:
 		query += " and src_type=%s"
