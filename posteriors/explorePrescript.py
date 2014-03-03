9c7dac2b86bfe68e043e10f8897e87bf46e01491
Rick Barber <barber5@stanford.edu>
2014-03-02 22:23:07 -0800

    no message

--- posteriors/explorePrescript.py
+++ posteriors/explorePrescript.py
@@ -77,7 +77,7 @@ def printTerms(pats, rnds):
 			increase = 9999
 		else:
 			increase = float(cnt - rndcnt)/float(rndcnt)
-		print '%s negated: %s history: %s\t%s\t%s\t%s' % (term, str(trm[1]), str(trm[2]), str(cnt), str(rndcnt), str(increase))
+		print '%s\t%s\t%s\t%s' % (trm[1], str(cnt), str(rndcnt), str(increase))
 	'''
 	for trm, cnt, in rnds.iteritems():
 		#print trm
