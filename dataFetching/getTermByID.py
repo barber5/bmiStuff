c39de867d5c89e2d57987a49e1f192529a1dc13a
Rick Barber <barber5@stanford.edu>
2014-03-08 17:51:21 -0800

    no message

--- dataFetching/getTermByID.py
+++ dataFetching/getTermByID.py
@@ -5,6 +5,7 @@ import sys, pprint
 (term_db, stride_db) = getDbs()
 
 def getTerm(term_id):	
+	print term_id
 	query = "SELECT term from terms where termid=%s"
 	rows = tryQuery(term_db, query, [term_id])
 	return rows[0][0].upper()
