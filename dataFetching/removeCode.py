eed47be8c5ef5e62e10e345181954fa293870fb5
Rick Barber <barber5@stanford.edu>
2014-03-09 22:34:59 -0700

    no message

--- dataFetching/removeCode.py
+++ dataFetching/removeCode.py
@@ -2,14 +2,20 @@ from queryByCui import getCuis, r, decomp, compIt
 import sys, json
 
 def remCode(code):
+	result = set([])
 	res = r.hget('codes', code)	
 	li = json.loads(decomp(res))	
 	for l in li:
-		print l
+		result.add(l)
+	return result
 
 def remAllBut():
 	codes = ['299', '135', 'random']
+	result = set([])
+	for code in codes:
+		result = result | remCode(code)
+	print len(result)
 	files = 'pancreatitis.txt'
 
 if __name__ == "__main__":
-	remCode(sys.argv[1])
+	remAllBut
