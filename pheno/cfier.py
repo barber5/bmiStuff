d81817da146d3a5abe735f15f97ccf8f75a512b1
Rick Barber <barber5@stanford.edu>
2014-03-09 21:21:36 -0700

    no message

--- pheno/cfier.py
+++ pheno/cfier.py
@@ -78,15 +78,18 @@ def vectorizePids(data, diagTerms=None, includeCid=False, includeLab=True, inclu
 
 
 		if diagTerms:
-			print >> sys.stderr, 'patient: '+str(pid)
+			print >> sys.stderr, 'patient: '+str(pid)+' label: '+str(label)
 			minOffset = float('inf')
 			for n in dd['notes']:
 				for t in n['terms']:
 					if str(t['tid']) in diagTerms and int(t['negated']) == 0 and int(t['familyHistory']) == 0:					
 						if float(n['timeoffset']) < minOffset:
 							minOffset = float(n['timeoffset'])
-					elif label == 0 and random.random() > .5:
-						minOffset = n['timeoffset']
+					elif label == 0:
+						if minOffset == float('inf'):
+							minOffset = n['timeoffset']
+						elif random.random() > .5:
+							minOffset = n['timeoffset']
 			print >> sys.stderr, 'minOffset: '+str(minOffset)			
 		if includeTerm:
 			for n in dd['notes']:
