a0b475df9e46a4ec57777b83e72db7a5626f70f1
Rick Barber <barber5@stanford.edu>
2014-03-09 22:36:45 -0700

    no message

--- dataFetching/removeCode.py
+++ dataFetching/removeCode.py
@@ -9,13 +9,32 @@ def remCode(code):
 		result.add(l)
 	return result
 
+
+def remFile(file):
+	result = set([])
+	with open(fName, 'r') as fi:
+		firstline = fi.readline()
+		while True:
+			line = fi.readline().strip()
+			if line == '':
+				break
+			lineArr = line.split('\t')
+			print lineArr
+			pid = lineArr[0]
+			result.add(pid)
+	return result
+
 def remAllBut():
 	codes = ['299', '135', 'random']
 	result = set([])
 	for code in codes:
 		result = result | remCode(code)
+
+	
+	files = ['pancreatitis.txt']
+	for fi in files:
+		result = result | remFile(fi)
 	print len(result)
-	files = 'pancreatitis.txt'
 
 if __name__ == "__main__":
 	remAllBut()
