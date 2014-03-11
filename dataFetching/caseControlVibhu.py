7d430a302a85c90e549485f7539b0197aa0eaece
Rick Barber <barber5@stanford.edu>
2014-03-11 14:21:30 -0700

    no message

--- dataFetching/caseControlVibhu.py
+++ dataFetching/caseControlVibhu.py
@@ -4,23 +4,25 @@ import sys
 
 def getPidsFromFile(fName, lo, hi):
 	i = 0
+	lines = []
 	with open(fName, 'r') as fi:
 		firstline = fi.readline()
-		while True:
-			i += 1
-			if i < lo:
-				continue
-			if i > hi:
-				break
+
+		while True:			
 			line = fi.readline().strip()
 			if line == '':
 				break
-			lineArr = line.split(' ')
-			print lineArr
-			pid = lineArr[1]
-			getVecForPid(pid)
-			pid = lineArr[2]
-			getVecForPid(pid)
+			lines.append(line)
+		
+
+	for i in range(lo-1, hi+1):
+		line = lines[i]
+		lineArr = line.split(' ')
+		print lineArr
+		pid = lineArr[1]
+		getVecForPid(pid)
+		pid = lineArr[2]
+		getVecForPid(pid)
 
 
 
