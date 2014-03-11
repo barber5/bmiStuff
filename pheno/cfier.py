45825f12076f3ea0e3c1c0c5f8d8acdfb2a9cfbd
Rick Barber <barber5@stanford.edu>
2014-03-10 20:36:03 -0700

    no message

--- pheno/cfier.py
+++ pheno/cfier.py
@@ -19,7 +19,7 @@ meta = {
 }
 
 stop_terms = range(10)
-stop_terms.extend([11, 13, 20, 21, 26, 39, 32, 40, 43, 46, 54, 18, 1028, 116, 339, 31])
+stop_terms.extend([11, 13, 20, 21, 26, 39, 32, 40, 43, 46, 54, 18])
 
 def getPidsFromFile(fname):
 	pids = {}
