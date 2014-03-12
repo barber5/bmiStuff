104d29502afd960618b86640cac194302e26cab0
Rick Barber <barber5@stanford.edu>
2014-03-12 09:39:20 -0700

    no message

--- diab.sh
+++ diab.sh
@@ -1,7 +1,7 @@
-python pheno/cfier.py diabcc.txt 1500 .2 diab.ignore diab/fimp_diab_part_nocids.txt -dt 10320 61045 2977 meds codes labs > diab/out_diab_part_nocids.txt
-python pheno/cfier.py diabcc.txt 1500 .2 diab.ignore diab/fimp_diab_part_cids.txt -dt 10320 61045 2977 meds codes labs cids > diab/out_diab_part_cids.txt
-python pheno/cfier.py diabcc.txt 1500 .2 diab.ignore diab/fimp_diab_part_terms.txt -dt 10320 61045 2977 meds codes labs terms > diab/out_diab_part_terms.txt
-python pheno/cfier.py diabcc.txt 1500 .2 diab.ignore diab/fimp_diab_part_cidsterms.txt -dt 10320 61045 2977 meds codes labs cids terms > diab/out_diab_part_cidsterms.txt
+python pheno/cfier.py diabcc.txt 1500 .2 diab.ignore diab/fimp_diab_part_nocids.txt -dt 400 2440 2056 15461 5831 7288 34537 8871 17635 3407 meds codes labs > diab/out_diab_part_nocids.txt
+python pheno/cfier.py diabcc.txt 1500 .2 diab.ignore diab/fimp_diab_part_cids.txt -dt 400 2440 2056 15461 5831 7288 34537 8871 17635 3407 meds codes labs cids > diab/out_diab_part_cids.txt
+python pheno/cfier.py diabcc.txt 1500 .2 diab.ignore diab/fimp_diab_part_terms.txt -dt 400 2440 2056 15461 5831 7288 34537 8871 17635 3407 meds codes labs terms > diab/out_diab_part_terms.txt
+python pheno/cfier.py diabcc.txt 1500 .2 diab.ignore diab/fimp_diab_part_cidsterms.txt -dt 400 2440 2056 15461 5831 7288 34537 8871 17635 3407 meds codes labs cids terms > diab/out_diab_part_cidsterms.txt
 python pheno/cfier.py diabcc.txt 1500 .2 diab.ignore diab/fimp_diab_full_nocids.txt meds codes labs > diab/out_diab_full_nocids.txt
 python pheno/cfier.py diabcc.txt 1500 .2 diab.ignore diab/fimp_diab_full_cids.txt meds codes labs cids > diab/out_diab_full_cids.txt
 python pheno/cfier.py diabcc.txt 1500 .2 diab.ignore diab/fimp_diab_full_terms.txt meds codes labs terms > diab/out_diab_full_terms.txt
