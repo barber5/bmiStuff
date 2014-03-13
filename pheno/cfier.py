77088113d358398e4fd709cf7062e3098ac157dd
Rick Barber <barber5@stanford.edu>
2014-03-12 20:00:08 -0700

    no message

--- pheno/cfier.py
+++ pheno/cfier.py
@@ -72,8 +72,8 @@ def getFeatName(metaDict, presentation=False):
 		else:
 			return 'code-presentation:'+str(v)
 	if metaDict['type'] == 'cid':
-		cui = metaDict['term']['cui']		
-		return 'cid:'+str(cid)
+		cui = metaDict['term']['cid']		
+		return 'cid:'+str(cui)
 
 # patients is pid -> {pid, src_type, labs -> [{age, , component, description, lid, line, ord, ord_num, proc, proc_cat, ref_high, ref_low, ref_norm, ref_unit, result_flag, result_inrange, src, timeoffset}], notes -> [{age, cpt, duration, icd9, nid, pid, src, src_type, timeoffset, year, terms -> [{cui, familyHistory, negated, nid, termid, tid}]}], prescriptions -> [{age, drug_description, ingr_set_id, order_status, pid, route, rxid, src, timeoffset}], visits -> [{age, cpt, duration, icd9, pid, src, src_type, timeoffset, year}] }
 def vectorizePids(data, diagTerms=None, includeCid=False, includeLab=True, includeTerm=True, includeCode=True, includePrescription=True, featureFilter={}, timeSlices=None):
