122b46579b1706128655b9fabe4ec36c2f3f3d07
Rick Barber <barber5@stanford.edu>
2014-03-19 16:43:02 -0700

    no message

--- fpmining/freqPatterns.py
+++ fpmining/freqPatterns.py
@@ -69,6 +69,7 @@ def mineDict(inp, threshold):
 				counts[ck] = 0
 			counts[ck] += 1
 	for ck, freq in counts.iteritems():
+		print freq
 		if float(freq) / float(len(inp.keys())) > float(threshold):
 			frequent.add(ck)
 
