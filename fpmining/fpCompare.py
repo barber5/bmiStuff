4d84eb6d398cf464796afb517b474a464293f17d
Rick Barber <barber5@stanford.edu>
2014-03-21 15:29:47 -0700

    no message

--- fpmining/fpCompare.py
+++ fpmining/fpCompare.py
@@ -4,5 +4,30 @@ sys.path.append(os.path.realpath('./tempClustering'))
 sys.path.append(os.path.realpath('../dataFetching'))
 sys.path.append(os.path.realpath('./dataFetching'))
 
+
+
+def loadFromFile(fileName):
+	result = {}
+	with open(fileName, 'r') as fi:
+		fi.readline()
+		while True:
+			line = fi.readline()
+			if line == '':
+				break
+			lineArr = line.split('\t')
+			nextRes = {
+				'freq': lineArr[0]
+				'pair': (lineArr[1][1:-1].split(', '), lineArr[2][1:-1].split(', '))
+				'desc': lineArr[3]
+			}
+	return result
+
+
+
+def comparePatterns(caseFile, controlFile):
+	cases = loadFromFile(caseFile)
+	controls = loadFromFile(controlFile)
+	pprint.pprint(cases)
+
 if __name__ == "__main__":
-	print 'hi'
+	print 'usage python fpCompare.py <caseFile> <controlFile>'
