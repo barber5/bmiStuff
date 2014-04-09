be0893fa57666171762351be0f260e9195c980a2
Rick Barber <barber5@stanford.edu>
2014-04-08 18:20:22 -0700

    no message

--- fpmining/mineConcepts.py
+++ fpmining/mineConcepts.py
@@ -96,11 +96,8 @@ def printFreq(freq, featIdx):
 	for k,v in freq.iteritems():				
 		if len(k) != 2:
 			continue
-		if k[0][0] == 'lab' or k[1][0] == 'lab':
-			print k
-			print featIdx[k[0]]
-			print featIdx[k[1]]
-		#print str(v)+'\t'+str(k[0])+'\t'+str(k[1])+'\t'+str(conce)
+		conce = featIdx[k[0]] + ' + ' + featIdx[k[1]]			
+		print str(v)+'\t'+str(k[0])+'\t'+str(k[1])+'\t'+str(conce)
 
 if __name__ == "__main__":
 	print >> sys.stderr, 'usage: <number> <patientFile> <threshold>'
