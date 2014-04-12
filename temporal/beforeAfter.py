e65277082e4f9faa67106db2e1362573ecd9dc58
Rick Barber <barber5@stanford.edu>
2014-04-11 21:57:10 -0700

    no message

--- temporal/beforeAfter.py
+++ temporal/beforeAfter.py
@@ -112,7 +112,7 @@ def printOffsets(featOffsets, enrichments):
 		case = enrichments[feat]['case']
 		control = enrichments[feat]['control']
 		desc = enrichments[feat]['description']
-		ofs = [str(s) for s in offsets]
+		ofs = [str(s) for s in sorted(offsets)]
 		print str(feat)+'\t'+str(enr)+'\t'+str(case)+'\t'+str(control)+'\t'+str(desc)+'\t'+str(', '.join(ofs))
 
 if __name__ == "__main__":
