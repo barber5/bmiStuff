86bd4738a3f2d389463b3320ac3cb70fba9c9083
Rick Barber <barber5@stanford.edu>
2014-04-11 21:46:29 -0700

    no message

--- temporal/beforeAfter.py
+++ temporal/beforeAfter.py
@@ -14,6 +14,7 @@ from mineConcepts import getFromFile
 # patients is pid -> {pid, src_type, labs -> [{age, , component, description, lid, line, ord, ord_num, proc, proc_cat, ref_high, ref_low, ref_norm, ref_unit, result_flag, result_inrange, src, timeoffset}], notes -> [{age, cpt, duration, icd9, nid, pid, src, src_type, timeoffset, year, terms -> [{cui, familyHistory, negated, nid, termid, tid}]}], prescriptions -> [{age, drug_description, ingr_set_id, order_status, pid, route, rxid, src, timeoffset}], visits -> [{age, cpt, duration, icd9, pid, src, src_type, timeoffset, year}] }
 def beforeAndAfter(enrichments, codes, patients):
 	total = 0
+	featOffsets = {}
 	for pid, resp in patients.iteritems():
 		myCodes = []
 		minOffset = float('inf')
@@ -28,11 +29,58 @@ def beforeAndAfter(enrichments, codes, patients):
 				if code in icd9sArr:										
 					if v['timeoffset'] < minOffset:
 						minOffset = v['timeoffset']
-		if minOffset < float('inf'):
-			print >> sys.stderr, str(minOffset)
-			total += 1		
+		if not minOffset < float('inf'):
+			continue
+		for n in dd['notes']:
+			delt = float(n['timeoffset']) - minOffset
+			for term in n['terms']:
+				cid = term['cid']
+				concept = term['concept']			
+				cidKey = ('cid', cid, term['negated'], term['familyHistory'])
+				if cidKey not in nextPerson:
+					nextPerson[cidKey] = 0
+					featIdx[cidKey] = term['concept']
+				if cidKey in enrichments:
+					if cidKey not in featOffsets:
+						featOffsets[cidKey] = []
 
-	print >> sys.stderr, str(total)
+					featOffsets[cidKey].append(delt)
+				nextPerson[cidKey] += 1
+		for l in dd['labs']:
+			if 'ord_num' not in l or not l['ord_num'] or l['ord_num'] == '':
+				continue
+			if 'result_flag' not in l or not l['result_flag'] or l['result_flag'] == '':
+				val = 'normal'
+			else:
+				val = l['result_flag']
+			delt = float(l['timeoffset']) - minOffset
+			labKey = ('lab', l['proc'], l['component'], val)
+			if labKey not in nextPerson:
+				nextPerson[labKey] = 0
+				featIdx[labKey] = getLab(l['component'])
+			if labKey in enrichments:
+				if labKey not in featOffsets:
+					featOffsets[labKey] = []
+				featOffsets[labKey].append(delt)
+			nextPerson[labKey] += 1
+
+
+
+		for p in dd['prescriptions']:	
+			delt = float(p['timeoffset']) - minOffset		
+			ings = getIngredients(p['ingr_set_id'])
+			for i in ings:				
+				ingKey = ('prescription', i)
+				if ingKey not in nextPerson:
+					nextPerson[ingKey] = 0
+					featIdx[ingKey] = getIngredient(i)
+				if ingKey in enrichments:
+					if ingKey not in featOffsets:
+						featOffsets[ingKey] = []
+					featOffsets[ingKey].append(delt)
+				nextPerson[ingKey] += 1
+	
+	return featOffsets
 
 def getEnrichments(enrFile):
 	enrichments = {}
