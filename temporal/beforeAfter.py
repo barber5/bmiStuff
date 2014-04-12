8160114934f51da36b7bc135b76a9bc0516b2580
Rick Barber <barber5@stanford.edu>
2014-04-11 20:32:20 -0700

    no message

--- temporal/beforeAfter.py
+++ temporal/beforeAfter.py
@@ -16,11 +16,13 @@ def beforeAndAfter(enrichments, codes, patients):
 	for pid, resp in patients.iteritems():
 		minOffset = float('inf')
 		for v in resp['visits']:
+			print v['icd9']
 			icd9s = v['icd9']
 			icd9sArr = icd9s.split(',')
 			icd9sArr = [str(i) for i in icd9sArr]
 			for code in codes:
-				if code in icd9sArr:
+				if code in icd9sArr:					
+					print type(code)
 					if v['timeoffset'] < minOffset:
 						minOffset = v['timeoffset']
 		print minOffset
