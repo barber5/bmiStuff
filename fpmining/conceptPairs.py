139fe90f3bceea5d9a6cc664b2baa3a3ab86693d
Rick Barber <barber5@stanford.edu>
2014-04-15 10:29:15 -0700

    no message

--- fpmining/conceptPairs.py
+++ fpmining/conceptPairs.py
@@ -24,7 +24,7 @@ def getPairs(num, patientFile, outFile):
 	conceptIdx = {}
 	pairIdx = {}
 	for pid, pat in pats.iteritems():		
-		concDict = patientToTimelessConcepts(pat, conceptIdx)
+		(concDict, conceptIdx) = patientToTimelessConcepts(pat)
 		concs = len(concDict.keys())
 		prs = concs*(concs-1)/2
 		keyser = concDict.keys()
@@ -46,7 +46,7 @@ def getPairs(num, patientFile, outFile):
 
 
 if __name__ == "__main__":
-	print >> sys.stderr, 'usage python conceptPairs.py <number of patient> <threshold> <patientFile> <outputFile>'
+	print >> sys.stderr, 'usage python conceptPairs.py <number of patient> <patientFile> <outputFile>'
 	freq = getPairs(int(sys.argv[1]), sys.argv[2], sys.argv[3])
 
 
