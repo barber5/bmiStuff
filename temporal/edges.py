c580e831776e76768f86f616d754abde1c513a10
Rick Barber <barber5@stanford.edu>
2014-04-16 14:52:22 -0700

    no message

--- temporal/edges.py
+++ temporal/edges.py
@@ -14,7 +14,7 @@ from beforeAfter import getEnrichments, getPatients
 
 
 # patients is pid -> {pid, src_type, labs -> [{age, , component, description, lid, line, ord, ord_num, proc, proc_cat, ref_high, ref_low, ref_norm, ref_unit, result_flag, result_inrange, src, timeoffset}], notes -> [{age, cpt, duration, icd9, nid, pid, src, src_type, timeoffset, year, terms -> [{cui, familyHistory, negated, nid, termid, tid}]}], prescriptions -> [{age, drug_description, ingr_set_id, order_status, pid, route, rxid, src, timeoffset}], visits -> [{age, cpt, duration, icd9, pid, src, src_type, timeoffset, year}] }
-def getEdges(enr, pats):
+def getEdges(enr, patients):
 	singletons = {}
 	pairs = {}
 	for pid, dd in patients.iteritems():											
