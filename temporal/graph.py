dee383cc97a0a3603a1650a3c43e5172b1781605
Rick Barber <barber5@stanford.edu>
2014-05-03 21:07:35 -0700

    no message

---
+++ temporal/graph.py
@@ -0,0 +1,124 @@
+import sys,os, pprint, json, random, pprint, math
+sys.path.append(os.path.realpath('../tempClustering'))
+sys.path.append(os.path.realpath('./tempClustering'))
+sys.path.append(os.path.realpath('../dataFetching'))
+sys.path.append(os.path.realpath('./dataFetching'))
+sys.path.append(os.path.realpath('../fpmining'))
+sys.path.append(os.path.realpath('./fpmining'))
+
+from queryByCui import r, decomp, compIt
+from getTermByID import getTerm, getTermCui, getIngredients, getIngredient, getLab, getConcept
+from ast import literal_eval as make_tuple
+from mineConcepts import getFromFile
+from beforeAfter import getEnrichments, getPatients
+from pygraphml.GraphMLParser import *
+from pygraphml.Graph import *
+from pygraphml.Node import *
+from pygraphml.Edge import *
+
+
+def inOutGraph(graphDict, gFile):
+	g = Graph()
+	nDict = {}
+	for node, meta in graphDict.iteritems():		
+		n = g.add_node(node[0][0]+': '+node[1])			
+		n['freq'] = meta['freq']
+		n['type'] = node[0][0]
+		n['enrichment'] = meta['enrichment']		
+		nDict[node] = n
+	for node, meta in graphDict.iteritems():
+		if node not in nDict:
+			continue
+		n = nDict[node]			
+		for node2, edgeMeta in meta['in'].iteritems():			
+			n2 = nDict[node2]
+			e = g.add_edge(n2, n, directed=True)
+			e['lift'] = edgeMeta['lift']
+			e['lambda'] = edgeMeta['lambda']
+			e['lambdaFirst'] = edgeMeta['lambdaFirst']		
+			e['avgOffset'] = edgeMeta['avgOffset']	
+			e['intersection'] = edgeMeta['intersection']
+	parser = GraphMLParser()
+	parser.write(g, gFile)
+
+
+def analyzeEdges(edges, intersectionCutoff=.05, cutoff=.01):
+	graph = {}
+	for pr, meta in edges.iteritems():
+		f1 = meta['f1']
+		f1 = (f1, meta['f1desc'])
+		if meta['f1freq'] < cutoff:
+			continue
+		
+		f2 = meta['f2']
+		f2 = (f2, meta['f2desc'])
+		if meta['f2freq'] < cutoff:
+			continue
+		if f1 not in graph:
+			graph[f1] = {
+				'in': {},
+				'out': {},
+				'adjacent': {},
+				'desc': meta['f1desc'],
+				'freq': meta['f1freq'],
+				'enrichment': meta['f1enrich']
+			}
+		if f2 not in graph:
+			graph[f2] = {
+				'in': {},
+				'out': {},
+				'adjacent': {},
+				'desc': meta['f2desc'],
+				'freq': meta['f2freq'],
+				'enrichment': meta['f2enrich']
+			}
+		
+		if meta['lift'] > 1 and meta['intersection'] > intersectionCutoff:
+			if meta['lambdaFirst'] > 0:
+				graph[f1]['out'][f2] = {
+					'lambda': meta['lambda'],
+					'lambdaFirst': meta['lambdaFirst'],
+					'lift': meta['lift'],
+					'avgOffset': meta['avgOffset'],
+					'intersection': meta['intersection']
+				}
+				graph[f2]['in'][f1] = {
+					'lambda': meta['lambda'],
+					'lambdaFirst': meta['lambdaFirst'],
+					'lift': meta['lift'],
+					'avgOffset': meta['avgOffset'],
+					'intersection': meta['intersection']
+				}
+			else:
+				graph[f2]['out'][f1] = {
+					'lambda': -meta['lambda'],
+					'lambdaFirst': -meta['lambdaFirst'],
+					'lift': meta['lift'],
+					'avgOffset': -meta['avgOffset'],
+					'intersection': meta['intersection']					
+				}
+				graph[f1]['in'][f2] = {
+					'lambda': -meta['lambda'],
+					'lambdaFirst': -meta['lambdaFirst'],
+					'lift': meta['lift'],
+					'avgOffset': -meta['avgOffset'],
+					'intersection': meta['intersection']
+				}
+		
+	return graph
+
+
+def edgesFromFile(edgeFile):
+	with open(edgeFile, 'r') as fi:
+		while True:
+			line = fi.readline()
+			if line == '':
+				break
+			print line
+
+if __name__ == "__main__":
+	print >> sys.stderr, 'usage <edgeFile> <outFile>'
+	edges = edgesFromFile(sys.argv[1])
+	graph = analyzeEdges(edges)
+	inOutGraph(graph, sys.argv[2])
+
